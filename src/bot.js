048fe19f9847c8ff7320b23a6837998279bb2009
raphael <raph.koh@gmail.com>
2017-08-16 02:24:34 -0400

    Fix errors

--- src/bot.js
+++ src/bot.js
@@ -110,7 +110,10 @@ function list(msg, callback) {
       db.getUserInfo(userId, (success, info) => {
         if (success) {
           const roles = (info.roles) ? info.roles.join(", ") : "N/A";
-          const skills = (info.skills) ? info.skills.join(", ") : "N/A";
+          const skills = (data.skills) ? data.skills.map(skill => {
+            if(skill.level) return `${skill.skill} (Level: ${skill.level})`;
+            else return `${skill.skill}`;
+          }).join(", ") : "N/A";
           const userName = info.username;
 
           // if valid username
