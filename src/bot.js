4a464826bac3aef5f5fca9068c7616100857d304
raphael <raph.koh@gmail.com>
2017-08-18 19:42:54 -0400

    Debug

--- src/bot.js
+++ src/bot.js
@@ -380,6 +380,7 @@ function displaySkillChoice(skills, callback) {
       format.formatSkillLvl(skill, actions, obj => next1(null, obj));
     });
   }, (err, attachments) => {
+    console.log(attachments);
     callback({
       text: `List the level of proficiency of each skill (${format.MAX_SKILL_LVL} = best):`,
       attachments: attachments
