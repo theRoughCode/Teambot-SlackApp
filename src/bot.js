34e5ae86700f2877757d271eafb59f6fdc5d2370
raphael <raph.koh@gmail.com>
2017-08-17 20:38:03 -0400

    Debug

--- src/bot.js
+++ src/bot.js
@@ -709,7 +709,42 @@ function contactUser(userId, matchId, type, responseUrl, callback) {
             console.log(attachments);
             SLACK.api("chat.postMessage", {
               "text": `Hi, ${res}!  ${info.username} would like to ${text}!\n Here's their information:`,
-              "attachments": 	[{"pretext": "pre-hello", "text": "text-world"}],
+              "attachments": 	[
+              {
+                "text": "Choose a game to play",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "game",
+                        "text": "Chess",
+                        "type": "button",
+                        "value": "chess"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Falken's Maze",
+                        "type": "button",
+                        "value": "maze"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Thermonuclear War",
+                        "style": "danger",
+                        "type": "button",
+                        "value": "war",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Wouldn't you prefer a good game of chess?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                  ]
+                }
+              ],
               "channel": userId,  //TODO change to matchId
               "username": BOT_NAME
             }, (err, response) => {
