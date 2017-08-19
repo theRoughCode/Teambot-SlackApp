c7eb42ec7f45832b8839d95854d5f029df6cd4fd
raphael <raph.koh@gmail.com>
2017-08-18 20:17:44 -0400

    Fix errors

--- src/bot.js
+++ src/bot.js
@@ -111,9 +111,14 @@ function parseIMsg(msg, callback) {
     setRoles(msg, actions[0].value, callback);
   } else if (callbackID === 'skills' || callbackID === 'skillsLvl') {
     callback(null);
-    console.log(actions);
-    if (callbackID === 'skills') updateSkillLevels(msg, actions[0].name, actions[0].selected_options[0].value, displaySkills);
-    else updateSkillLevels(msg, actions[0].name, actions[0].value, displaySkillChoice);
+
+    if (callbackID === 'skills') {
+      const value = (actions[0].selected_options) ? actions[0].selected_options[0].value : actions[0].value;
+
+      updateSkillLevels(msg, actions[0].name, value, displaySkills);
+      
+    } else updateSkillLevels(msg, actions[0].name, actions[0].value, displaySkillChoice);
+
   } else if (callbackID === 'discover') { // turn on discoverability
     if (actions[0].name === "yes") setDiscoverable(msg, true, actions[0].value, callback);
     else callback("All the best team-hunting! :smile:");
