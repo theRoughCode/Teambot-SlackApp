c8619ae332a51b5b363d6d8d72b7927844f80687
raphael <raph.koh@gmail.com>
2017-08-19 01:14:03 -0400

    Edit formatting

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ routes.post('/slash', function(req, res) {
 
 // interactive messages
 routes.post('/interact', function(req, res) {
-  console.log(req.body.message_ts);
+  console.log(req.body);
   /*
   {
     "actions": [{"name","type","value"/"selected_options:[{"value"}]"}],
