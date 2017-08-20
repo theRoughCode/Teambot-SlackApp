5be5a8ee6b33a7e470907f24b4f4b643b72a17fd
raphael <raph.koh@gmail.com>
2017-08-20 12:23:12 -0400

    Fix errors

--- src/bot.js
+++ src/bot.js
@@ -630,7 +630,7 @@ function editUserType(msg, type, callback) {
               // prevents roles from writing over this msg
             //  sendMsgToUrl({ text: `:pencil: You are now looking for ${str}.` }, responseUrl);
               sendMsgToChannel(userId, msg.channel.name, `:pencil: You are now looking for ${str}.`);
-              setRoles(msg, null, true, () => {});
+              setTimeout(() => setRoles(msg, null, true, () => {}), 1500);
             } else {
               format.displayErrorMsg("Failed to update member", msg => {
                 return sendMsgToUrl({
