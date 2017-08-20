85398abaf1871db5b1d170d8a94d6866b326aaa3
raphael <raph.koh@gmail.com>
2017-08-20 13:55:22 -0400

    Edit help msg

--- src/format.js
+++ src/format.js
@@ -4,7 +4,7 @@ const MAX_SKILL_LVL = 4;
 
 // list commands
 function helpMsg(callback) {
-  callback({ text: "List of commands:\n  `/teambot` or `/teambot start` to edit your team profile!\n  `/teambot info` to update your description and let others know more about you and what you want to work on! (*Limit: 200 characters*)\n `/teambot list (members/teams)` to display the list of discoverable users\n  `/teambot remove` to remove your information from the database" });
+  callback({ text: "List of commands:\n  `/teambot` or `/teambot start` to view your team formation dashboard and edit your team profile!\n  `/teambot info` to update your description and let others know more about you and what you want to work on! (*Limit: 200 characters*)\n `/teambot list (members/teams)` to display the list of discoverable users\n  `/teambot remove` to remove your information from the database" });
 }
 
 // Welcome new users
