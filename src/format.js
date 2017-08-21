2b67db8ca698590dd28a67e4ffa037864f7f8b85
raphael <raph.koh@gmail.com>
2017-08-21 01:27:49 -0400

    Debug

--- src/format.js
+++ src/format.js
@@ -54,7 +54,7 @@ function formatMatches(sortedMatches, type, callback) {
   async.map(sortedMatches, (match, next) => {
     formatUser(match.user_id, match.user_name, match.roles, match.skills, match.info, obj => {
       if (match.requested) {
-        obj.fields.push({ "text": "Request sent! :white_check_mark:" });
+        obj.fields.push({ "value": "Request sent! :white_check_mark:" });
       } else {
         var text = (type === "team") ? "Request to join" : "Invite as member";
         obj["callback_id"] = "request";
